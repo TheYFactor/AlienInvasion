5ecbcbfcb374e1356b1c9bdcb3935d5a78a32cb5
TheYFactor <96528893+TheYFactor@users.noreply.github.com>
2022-01-22 19:55:17 +0200

    Update alien_invasion.py

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -252,6 +252,6 @@ class AlienInvasion:
 
 
 if __name__ == '__main__':
-	# Make a game instance, and run the game.
-	ai = AlienInvasion()
-	ai.run_game()
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
